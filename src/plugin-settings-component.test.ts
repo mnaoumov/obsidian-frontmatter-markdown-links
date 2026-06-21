4082fc4e199641547e3dab2832fc870304cb4745
Michael Naumov <mnaoumov@gmail.com>
2026-06-21 12:00:21 -0600

    test(settings-component): drive the real PluginSettingsComponentBase

--- src/plugin-settings-component.test.ts
+++ src/plugin-settings-component.test.ts
@@ -1,42 +1,21 @@
+import type { DataHandler } from 'obsidian-dev-utils/obsidian/data-handler';
+import type { PluginEventSource } from 'obsidian-dev-utils/obsidian/plugin/plugin-event-source';
+
 import { strictProxy } from 'obsidian-dev-utils/strict-proxy';
 import {
   describe,
   expect,
-  it,
-  vi
+  it
 } from 'vitest';
 
-interface MockConstructorParams {
-  readonly pluginSettingsClass: new () => unknown;
-}
-
-const PluginSettingsComponentBaseMock = vi.hoisted(() =>
-  class {
-    public readonly defaultSettings: unknown;
-
-    public constructor(params: MockConstructorParams) {
-      this.defaultSettings = new params.pluginSettingsClass();
-    }
-  }
-);
-
-vi.mock('obsidian-dev-utils/obsidian/components/plugin-settings-component', () => ({
-  PluginSettingsComponentBase: PluginSettingsComponentBaseMock
-}));
-
-vi.mock('./plugin-settings.ts', () => ({
-  PluginSettings: class MockPluginSettings {
-    public shouldHandleRenames = true;
-    public shouldShowInitializationNotice = true;
-  }
-}));
-
-// eslint-disable-next-line import-x/first, import-x/imports-first -- vi.mock must precede imports.
 import { PluginSettingsComponent } from './plugin-settings-component.ts';
 
 describe('PluginSettingsComponent', () => {
-  it('should create default settings', () => {
-    const component = new PluginSettingsComponent(strictProxy<ConstructorParameters<typeof PluginSettingsComponent>[0]>({}));
+  it('should create default settings from the PluginSettings class', () => {
+    const component = new PluginSettingsComponent({
+      dataHandler: strictProxy<DataHandler>({}),
+      pluginEventSource: strictProxy<PluginEventSource>({})
+    });
     const settings = component.defaultSettings;
 
     expect(settings).toBeDefined();
